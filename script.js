4cc281cb83690cc7097cbe81a80ad9fbacd9ce88
susom <susomshrestha@gmail.com>
2021-06-01 07:17:36 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log(`'15' - 5 = ` ,'15' - 5);
+console.log(`'15' * 5 = ` ,'15' * 5);
+console.log(`'15' / 5 = ` ,'15' / 5);
+console.log(`-----------------------------`);
+console.log(`'aa' - 5 = ` ,'a' - 5);
+console.log(`'aa' - 5 = ` ,'a' - 5);
+console.log(`'aa' - 5 = ` ,'a' - 5);
